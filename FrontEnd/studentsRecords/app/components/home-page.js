45017576f8de2cb9e18be4fc36d7690ed8376a5b
NickVanBommel <nvanbomm@uwo.ca>
2017-01-30 14:19:39 -0500

    Added System Codes tab to main page

--- FrontEnd/studentsRecords/app/components/home-page.js
+++ FrontEnd/studentsRecords/app/components/home-page.js
@@ -18,24 +18,34 @@ export default Ember.Component.extend({
   isHomeShowing: true,
   isStudentsRecordsDataEntry: false,
   isAboutShowing: false,
+  isSystemCodesShowing: false,
 
   actions: {
     home () {
       this.set('isHomeShowing', true);
       this.set('isStudentsRecordsDataEntry', false);
       this.set('isAboutShowing', false);
+      this.set('isSystemCodesShowing', false);
     },
 
     studentsDataEntry (){
       this.set('isHomeShowing', false);
       this.set('isStudentsRecordsDataEntry', true);
       this.set('isAboutShowing', false);
+      this.set('isSystemCodesShowing', false);
     },
 
     about (){
       this.set('isHomeShowing', false);
       this.set('isStudentsRecordsDataEntry', false);
       this.set('isAboutShowing', true);
+      this.set('isSystemCodesShowing', false);
+    },
+    SystemCodes (){
+      this.set('isHomeShowing', false);
+      this.set('isStudentsRecordsDataEntry', false);
+      this.set('isAboutShowing', false);
+      this.set('isSystemCodesShowing', true);
     }
   }
 });
