a7c6a204b3c3d89e30c8aa27ee9027e264432c8a
mromao <mromao@uwo.ca>
2017-01-31 15:06:52 -0500

    Updated studentRecordsDB in the backend BackEnd/models/studentsRecords/studentRecordsDB.js

--- BackEnd/studentsRecords/models/studentsRecordsDB.js
+++ BackEnd/studentsRecords/models/studentsRecordsDB.js
@@ -5,10 +5,11 @@ var studentsSchema = mongoose.Schema(
         number: String,
         firstName: String,
         lastName: String,
-        gender: Number,
+        //gender: Number,
         DOB: Date,
         photo: String,
-        resInfo: {type: mongoose.Schema.ObjectId, ref: 'Residencies'}
+        resInfo: {type: mongoose.Schema.ObjectId, ref: 'Residencies'},
+        genInfo: {type: mongoose.Schema.ObjectId, ref: 'Genders'}
     }
 );
 studentsSchema.plugin(mongoosePaginate);
@@ -20,8 +21,16 @@ var residencySchema = mongoose.Schema(
     }
 );
 
+var genderSchema = mongoose.Schema(
+    {
+        name: String,
+        students: [{type: mongoose.Schema.ObjectId, ref: ('Students')}]
+    }
+)
+
 var Students = mongoose.model('student', studentsSchema);
 var Residencies = mongoose.model('residency', residencySchema);
+var Genders = mongoose.model('gender',genderSchema);
 
 
 mongoose.connect('mongodb://localhost/studentsRecords');
@@ -31,6 +40,7 @@ db.once('open', function() {
 
     exports.Students = Students;
     exports.Residencies = Residencies;
+    exports.Genders = Genders;
 
 });
 
