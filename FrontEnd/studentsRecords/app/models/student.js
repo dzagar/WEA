861fbc71103fea0a0290d39ff7e63b42da14dfa2
mromao <mromao@uwo.ca>
2017-02-02 15:49:15 -0500

    Also modified the student.js file to include our new gender model

--- FrontEnd/studentsRecords/app/models/student.js
+++ FrontEnd/studentsRecords/app/models/student.js
@@ -7,6 +7,7 @@ export default DS.Model.extend({
   gender: DS.attr('number'),
   DOB: DS.attr('date'),
   photo: DS.attr(),
-  resInfo: DS.belongsTo('residency')
+  resInfo: DS.belongsTo('residency'),
+  genInfo: DS.belongsTo('gender')
 
 });
