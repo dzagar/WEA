0396f5512ec1299c1dfaca64a8d597384b850976
mromao <mromao@uwo.ca>
2017-02-03 16:48:34 -0500

    Added Some Cool Features with Residency

--- FrontEnd/studentsRecords/app/components/delete-residency.js
+++ FrontEnd/studentsRecords/app/components/delete-residency.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    store: Ember.inject.service(),
+	residency: null,
+	showWindow: null,
+
+	actions: {
+		deleteResidency: function(){
+			this.get('residency').destroyRecord();
+			this.set('showWindow', false);
+			Ember.$('.ui.modal').modal('hide');
+      		Ember.$('.ui.modal').remove();
+
+		},
+		cancel: function(){
+			this.set('showWindow', false);
+			Ember.$('.ui.modal').modal('hide');
+      		Ember.$('.ui.modal').remove();
+		}
+	},
+
+    didRender() {
+    Ember.$('.ui.modal')
+      .modal({
+        closable: false,
+      })
+      .modal('show');
+  }
 });
