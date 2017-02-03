257faf44d504e536f5b9aed411856d424a8f4bcd
Tom Machado <tmachad3@uwo.ca>
2017-02-03 14:06:51 -0500

    Added gender display table to system codes page

--- FrontEnd/studentsRecords/app/components/system-codes.js
+++ FrontEnd/studentsRecords/app/components/system-codes.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    store: Ember.inject.service(),
+    residencyModel: null,
+    genderModel: null,
 
+    init() {
+        this._super(...arguments);
+        var self = this;
+        
+        // load Residency data model
+        this.get('store').findAll('residency').then(function (records) {
+            self.set('residencyModel', records);
+        });
+
+        // load Gender data model 
+        this.get('store').findAll('gender').then(function (records) {
+            self.set('genderModel',records);
+        });
+    },
     didRender() {
     Ember.$('.menu .item').tab();
     }
