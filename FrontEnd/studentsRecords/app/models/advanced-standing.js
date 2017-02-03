03d276cb8ebccd7af470fdccace9b1bd19f6db3a
dzagar <dzagar@uwo.ca>
2017-02-03 14:02:46 -0500

    Updated AdvancedStanding and Scholarship models

--- FrontEnd/studentsRecords/app/models/advanced-standing.js
+++ FrontEnd/studentsRecords/app/models/advanced-standing.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+	student: DS.belongsTo('student'),
+	name: DS.attr(),
+	description: DS.attr(),
+	units: DS.attr(),
+	grades: DS.attr(),
+	from: DS.attr()
 });
--- FrontEnd/studentsRecords/app/models/scholarship.js
+++ FrontEnd/studentsRecords/app/models/scholarship.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+	student: DS.belongsTo('student'),
+	note: DS.attr()
 });
