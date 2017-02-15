6db73eb5a971b8d9b9d88540371497f1a189e6f0
NickVanBommel <nvanbomm@uwo.ca>
2017-02-15 16:11:25 -0500

    added import function and import of excel reader

--- FrontEnd/studentsRecords/app/components/initial-import.js
+++ FrontEnd/studentsRecords/app/components/initial-import.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import XLSX from 'npm:xlsx-browserify-shim';
 
 export default Ember.Component.extend({
 
@@ -10,6 +11,25 @@ export default Ember.Component.extend({
 	actions: {
 		showEraseDataModal: function(){
 			this.set('showDeleteConfirmation', true);
+		}
+		import() {
+
+			var files = $("#newFile")[0].files;
+			var i,f;
+			for (i = 0; i != files.length; ++i) {
+			    f = files[i];
+			    console.log(f);
+			    var reader = new FileReader();
+			    var name = f.name;
+			    reader.onload = function(e) {
+			    	var data = e.target.result;
+			    	var workbook;
+			    	workbook = XLSX.read(data, {type: 'binary'});
+			    	/* DO SOMETHING WITH workbook HERE */
+			    	console.log(workbook.SheetNames[0]);
+		    	};
+		    reader.readAsBinaryString(f);
+		  }
 		}
 		
 	}
