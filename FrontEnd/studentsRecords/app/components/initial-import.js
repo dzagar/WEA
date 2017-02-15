91cfd26b80bbb04992e268af9b8e00b2236d4569
dzagar <dzagar@uwo.ca>
2017-02-15 14:11:26 -0500

    Syntax error

--- FrontEnd/studentsRecords/app/components/initial-import.js
+++ FrontEnd/studentsRecords/app/components/initial-import.js
@@ -2,10 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	store: Ember.inject.service();
+	store: Ember.inject.service(),
 	notDONE: null,
 
-	
+	actions: {
+
+	}
 
 
 
