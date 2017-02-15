9067faeeef36c0d6c03292e072f8d05aa0e3dc8e
dzagar <dzagar@uwo.ca>
2017-02-15 14:49:35 -0500

    Got cancel to go back to original initial import btn state

--- FrontEnd/studentsRecords/app/components/initial-import.js
+++ FrontEnd/studentsRecords/app/components/initial-import.js
@@ -5,13 +5,11 @@ export default Ember.Component.extend({
 	store: Ember.inject.service(),
 	notDONE: null,
 	showDeleteConfirmation: false,
-	showInitialDataImportBtn: true,
 	importData: false,
 
 	actions: {
 		showEraseDataModal: function(){
 			this.set('showDeleteConfirmation', true);
-			this.set('showInitialDataImportBtn', false);
 		}
 		
 	}
