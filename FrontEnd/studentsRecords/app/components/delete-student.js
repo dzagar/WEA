f10879c6eb58bf8287d724b70185b1b239a3fb88
darcese <darcese@uwo.ca>
2017-02-15 16:09:41 -0500

    changed totalStudent count when adding/deleting student

--- FrontEnd/studentsRecords/app/components/delete-student.js
+++ FrontEnd/studentsRecords/app/components/delete-student.js
@@ -6,11 +6,13 @@ export default Ember.Component.extend({
 	showWindow: null,
 	store: Ember.inject.service(),
 	student: null,
+	total: 0,
 
 	actions: {
 		deleteStudent: function(student){
 			this.get('student').destroyRecord();
 			this.set('INDEX', this.get('INDEX')+1);
+			this.set('total', this.get('total')-1);
 			this.set('showWindow', false);
 			Ember.$('.ui.modal').modal('hide');
       		Ember.$('.ui.modal').remove();
