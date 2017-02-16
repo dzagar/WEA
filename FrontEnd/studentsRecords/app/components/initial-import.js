40c824f1c2729d88b41925e4127ebaf50cac388a
mromao <mromao@uwo.ca>
2017-02-16 11:08:40 -0500

    Added Database Import Verification

--- FrontEnd/studentsRecords/app/components/initial-import.js
+++ FrontEnd/studentsRecords/app/components/initial-import.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
 	store: Ember.inject.service(),
 	showDeleteConfirmation: false,
 	importData: false,
+	index: null,
 
 	actions: {
 		showEraseDataModal: function(){
@@ -23,11 +24,159 @@ export default Ember.Component.extend({
 			    	var data = e.target.result;
 			    	var workbook;
 			    	workbook = XLSX.read(data, {type: 'binary'});
-			    	/* DO SOMETHING WITH workbook HERE */
-			    	console.log(workbook.SheetNames[0]);
+					var currentWorkSheet=workbook.SheetNames[0];
+					var worksheet=workbook.Sheets[currentWorkSheet];
+			    	switch(index)
+					{
+						case 0: genderVerfication();
+								break;
+						case 1:	residencyVerification();
+								break;
+						case 2:	termCodeVerification();
+								break;
+						case 3:	courseCodeVerification();
+								break;
+						case 4:	studentVerification();
+								break;
+					}
+			    	console.log(currentWorkSheet);
 		    	};
 		    reader.readAsBinaryString(f);
 			}
+		},
+
+		genderVerification()
+		{
+			var currentString=worksheet['A1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='NAME')
+			{
+
+			}
+		},
+
+		residencyVerification()
+		{
+			var currentString=worksheet['A1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='NAME')
+			{
+				DisplayErrorMessage();
+			}
+		},
+
+		termCodeVerification()
+		{
+			var currentString=worksheet['A1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='NAME')
+			{
+				DisplayErrorMessage();
+			}
+		},
+
+		courseCodeVerification()
+		{
+			var currentString=worksheet['A1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='STUDENTNUMBER')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['B1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='TERM')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['C1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='COURSELETTER')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['D1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='COURSENUMBER')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['E1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='SECTION')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['F1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='GRADE')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['G1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='NOTE')
+			{
+				DisplayErrorMessage();
+			}
+		},
+
+		studentVerification()
+		{
+			currentString=worksheet['A1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='STUDENTNUMBER')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['B1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='FIRSTNAME')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['C1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='LASTNAME')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['D1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='GENDER')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['E1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='DOB')
+			{
+				DisplayErrorMessage();
+			}
+
+			currentString=worksheet['F1'];
+			currentString=currentString.toUpperCase();
+			if(currentString!='RESIDENCY')
+			{
+				DisplayErrorMessage();
+			}
+
+		},
+
+		DisplayErrorMessage()
+		{
+			console.log('You fucked up the import bruh!');
 		}
+
 	}
 });
