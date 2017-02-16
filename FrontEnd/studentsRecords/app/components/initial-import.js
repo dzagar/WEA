f845478f7939f80f24cb92b1caef00c53403d836
dzagar <dzagar@uwo.ca>
2017-02-16 14:41:38 -0500

    Added Term Code model front end, added import fcn for Residency + Term Code

--- FrontEnd/studentsRecords/app/components/initial-import.js
+++ FrontEnd/studentsRecords/app/components/initial-import.js
@@ -192,73 +192,144 @@ export default Ember.Component.extend({
 
 					switch(currentIndex)
 					{
-						case 0:
-						if (genderVerfication(worksheet))
-						{
-							var rollBackImport = false;
-							var doneImporting = false;
-							var gendersToImport = [];
-							var uniqueGenderNames = [];
-							for (var i = 2; !doneImporting; i++)
-							{
-								//get the next gender name
-								var gender = worksheet['A' + i];
-								//if the gender exists
-								if (gender)
-								{
-									//gets the genderNameString
-									var genderName = gender.v;
-									//if the gender has already been added
-									if (uniqueGenderNames.includes(genderName))
-									{
-										DisplayErrorMessage("Import cancelled. Your excel sheet contains duplicate gender names '" + genderName + "'");
-										rollBackImport = true;
+						case 0: //Gender
+							if (genderVerification(worksheet)){
+								var rollBackImport = false;
+								var doneImporting = false;
+								var gendersToImport = [];
+								var uniqueGenderNames = [];
+								for (var i = 2; !doneImporting; i++){
+									//get the next gender name
+									var gender = worksheet['A' + i];
+									//if the gender exists
+									if (gender){
+										//gets the genderNameString
+										var genderName = gender.v;
+										//if the gender has already been added
+										if (uniqueGenderNames.includes(genderName)){
+											DisplayErrorMessage("Import cancelled. Your excel sheet contains duplicate gender names '" + genderName + "'");
+											rollBackImport = true;
+											doneImporting = true;
+										} else { //create new gender object
+											gendersToImport[i - 2] = self.get('store').createRecord('gender', 
+											{
+												name: genderName
+											});
+										}
+									} else {
 										doneImporting = true;
-									}
-									//create new gender object
-									else
-									{
-										gendersToImport[i - 2] = self.get('store').createRecord('gender', 
-										{
-											name: genderName
-										});
+										//if no gender was imported
+										if (i == 2) {
+											rollBackImport = true;
+											DisplayErrorMessage("File does not contain any Values...")
+										}
 									}
 								}
-								else
-								{
-									doneImporting = true;
-									//if no gender was imported
-									if (i == 2)
-									{
-										rollBackImport = true;
-										DisplayErrorMessage("File does not contain any Values...")
-
+								//delete genders from the store
+								if (rollBackImport) {
+									for (var i = 0; i < gendersToImport.length; i++) {
+										gendersToImport[i].deleteRecord();
+									}
+								} else {
+									for (var i = 0; i < gendersToImport.length; i++) {
+										console.log("trying to save");
+										gendersToImport[i].save();
 									}
 								}
-								//if the gender is a duplicate, cancel import and display error message
 							}
-							//delete genders from the store
-							if (rollBackImport)
-							{
-								for (var i = 0; i < gendersToImport.length; i++)
-								{
-									gendersToImport[i].deleteRecord();
+							break;
+						case 1: //Residency
+							if (residencyVerification(worksheet)) {
+								var rollBackImport = false;
+								var doneImporting = false;
+								var residenciesToImport = [];
+								var uniqueResidencyNames = [];
+								for (var i = 2; !doneImporting; i++) {
+									//get the next residency name
+									var residency = worksheet['A' + i];
+									//if the residency exists
+									if (residency) {
+										//gets the residencyNameString
+										var residencyName = residency.v;
+										//if the residency has already been added
+										if (uniqueResidencyNames.includes(residencyName)) {
+											DisplayErrorMessage("Import cancelled. Your excel sheet contains duplicate residency names '" + residencyName + "'");
+											rollBackImport = true;
+											doneImporting = true;
+										} else { //create new residency object
+											residenciesToImport[i - 2] = self.get('store').createRecord('residency', 
+											{
+												name: residencyName
+											});
+										}
+									} else {
+										doneImporting = true;
+										//if no residency was imported
+										if (i == 2) {
+											rollBackImport = true;
+											DisplayErrorMessage("File does not contain any Values...")
+										}
+									}
+								}
+								//delete residencies from the store
+								if (rollBackImport) {
+									for (var i = 0; i < residenciesToImport.length; i++) {
+										residenciesToImport[i].deleteRecord();
+									}
+								} else { //save residencies to back-end
+									for (var i = 0; i < residenciesToImport.length; i++) {
+										console.log("trying to save");
+										residenciesToImport[i].save();
+									}
 								}
 							}
-							else
-							{
-								for (var i = 0; i < gendersToImport.length; i++)
-								{
-									console.log("trying to save");
-									gendersToImport[i].save();
+							break;
+						case 2:
+							if (termCodeVerification(worksheet)) {
+								var rollBackImport = false;
+								var doneImporting = false;
+								var termCodesToImport = [];
+								var uniqueTermCodeNames = [];
+								for (var i = 2; !doneImporting; i++) {
+									//get the next term code name
+									var termCode = worksheet['A' + i];
+									//if the term code exists
+									if (termCode) {
+										//gets the termCodeNameString
+										var termCodeName = termCode.v;
+										//if the term code has already been added
+										if (uniqueTermCodeNames.includes(termCodeName)) {
+											DisplayErrorMessage("Import cancelled. Your excel sheet contains duplicate term code names '" + termCodeName + "'");
+											rollBackImport = true;
+											doneImporting = true;
+										} else { //create new term code object
+											termCodesToImport[i - 2] = self.get('store').createRecord('term-code', 
+											{
+												name: termCodeName
+											});
+										}
+									} else {
+										doneImporting = true;
+										//if no term code was imported
+										if (i == 2) {
+											rollBackImport = true;
+											DisplayErrorMessage("File does not contain any Values...")
+										}
+									}
+								}
+								//delete term codes from the store
+								if (rollBackImport) {
+									for (var i = 0; i < termCodesToImport.length; i++) {
+										termCodesToImport[i].deleteRecord();
+									}
+								} else {
+									for (var i = 0; i < termCodesToImport.length; i++) {
+										console.log("trying to save");
+										termCodesToImport[i].save();
+									}
 								}
 							}
-						}
-						break;
-						case 1:	residencyVerification(worksheet);
-						break;
-						case 2:	termCodeVerification(worksheet);
-						break;
+							break;
 						case 3:	courseCodeVerification(worksheet);
 						break;
 						case 4:	studentVerification(worksheet);
