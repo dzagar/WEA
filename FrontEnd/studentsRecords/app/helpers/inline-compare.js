450cc94667cc58680103bce0162f75cbdca77329
Tom Machado <tmachad3@uwo.ca>
2017-02-17 18:51:02 -0500

    Added comparison helper and made page menu better

---
+++ FrontEnd/studentsRecords/app/helpers/inline-compare.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export function inlineCompare(params/*, hash*/) {
+  let op1 = params[0];
+  let operation = params[1];
+  let op2 = params[2];
+
+  switch (operation){
+    case '==':
+      return op1 == op2;
+    case '!=':
+      return op1 != op2;
+    case '===':
+      return op1 === op2;
+    case '!==':
+      return op1 !== op2;
+    case '>':
+      return op1 > op2;
+    case '>=':
+      return op1 >= op2;
+    case '<':
+      return op1 < op2;
+    case '<=':
+      return op1 <= op2;
+    case '&&':
+      return op1 && op2;
+    case '||':
+      return op1 || op2;
+  }
+
+  return false;
+}
+
+export default Ember.Helper.helper(inlineCompare);
