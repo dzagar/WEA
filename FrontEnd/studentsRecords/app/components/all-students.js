cdd27718839cd0fb20825f3d9cf35fad893f8739
Tom Machado <tmachad3@uwo.ca>
2017-02-18 10:45:21 -0500

    Alphabetized actions a variables in all-students

--- FrontEnd/studentsRecords/app/components/all-students.js
+++ FrontEnd/studentsRecords/app/components/all-students.js
@@ -3,24 +3,35 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   
   currentIndex: null,
-  limit: 10,
+  currentStudent: null,
+  offset: 0,
+  oldIndex: 0,
+  oldOffset: 0,
+  oldStudent: null,
+  pageNumber: 0,
+  pageSize: 10,
   showAllStudents: true,
   showMenuBar: false,
-  offset: 0,
-  pageSize: 10,
   store: Ember.inject.service(),
   studentsRecords: null,
   totalPages: 0,
-  pageNumber: 0,
-  oldOffset: 0,
-  oldIndex: 0,
-  currentStudent: null,
-  oldStudent: null,
 
   actions: {
 
+    back() {
+      this.set('offset', this.get('oldOffset'));
+      this.set('index', this.get('oldIndex'));
+      this.set('currentStudent', this.get('currentStudent'));
+      this.set('showAllStudents', false);
+      this.set('showMenuBar', true);
+    },
+
     firstPage() {
       this.get('changeOffset')(0, false);
+    },
+
+    lastPage() {
+      this.get('changeOffset')((this.get('totalPages') - 1) * this.get('pageSize'), false);
     },
 
     nextPage() {
@@ -31,24 +42,12 @@ export default Ember.Component.extend({
       this.get('changeOffset')(-this.get('pageSize'), true);
     },
 
-    lastPage() {
-      this.get('changeOffset')((this.get('totalPages') - 1) * this.get('pageSize'), false);
-    },
-
     selectStudent(student, index) {
       this.set('showAllStudents', false);
       this.set('showMenuBar', true);
       this.set('currentIndex', index);
       this.set('currentStudent', student);
     },
-
-    back() {
-      this.set('offset', this.get('oldOffset'));
-      this.set('index', this.get('oldIndex'));
-      this.set('currentStudent', this.get('currentStudent'));
-      this.set('showAllStudents', false);
-      this.set('showMenuBar', true);
-    }
   },
 
   init() {
