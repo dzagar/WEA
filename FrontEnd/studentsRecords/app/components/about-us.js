c698f6b8eda2a70073b4dffbf1a1bbd71f9a39cc
dzagar <dzagar@uwo.ca>
2017-03-03 12:50:21 -0500

    Added headshots, started new About Us page

--- FrontEnd/studentsRecords/app/components/about-us.js
+++ FrontEnd/studentsRecords/app/components/about-us.js
@@ -1,4 +1,79 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	danielaHover: false,
+	tomHover: false,
+	mikeHover: false,
+	nickHover: false,
+	danaHover: false,
+
+	actions: {
+		onDanielaHover: function(){
+			if (this.get('danielaHover')){
+				this.set('danielaHover', false);
+			} else {
+				this.set('danielaHover', true);
+				this.set('tomHover', false);
+				this.set('mikeHover', false);
+				this.set('nickHover', false);
+				this.set('danaHover', false);
+			}
+		},
+
+		onTomHover: function(){
+			if (this.get('tomHover')){
+				this.set('tomHover', false);
+			} else {
+				this.set('danielaHover', false);
+				this.set('tomHover', true);
+				this.set('mikeHover', false);
+				this.set('nickHover', false);
+				this.set('danaHover', false);
+			}
+		},
+
+		onMikeHover: function(){
+			if (this.get('mikeHover')){
+				this.set('mikeHover', false);
+			} else {
+				this.set('danielaHover', false);
+				this.set('tomHover', false);
+				this.set('mikeHover', true);
+				this.set('nickHover', false);
+				this.set('danaHover', false);
+			}
+		},
+
+		onNickHover: function(){
+			if (this.get('nickHover')){
+				this.set('nickHover', false);
+			} else {
+				this.set('danielaHover', false);
+				this.set('tomHover', false);
+				this.set('mikeHover', false);
+				this.set('nickHover', true);
+				this.set('danaHover', false);
+			}
+		},
+
+		onDanaHover: function(){
+			if (this.get('danaHover')){
+				this.set('danaHover', false);
+			} else {
+				this.set('danielaHover', false);
+				this.set('tomHover', false);
+				this.set('mikeHover', false);
+				this.set('nickHover', false);
+				this.set('danaHover', true);
+			}
+		},
+
+		onHoverAway: function(){
+			this.set('danielaHover', false);
+			this.set('tomHover', false);
+			this.set('mikeHover', false);
+			this.set('nickHover', false);
+			this.set('danaHover', false);
+		}
+	}
 });
