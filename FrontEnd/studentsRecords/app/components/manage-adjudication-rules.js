4db430d222930d0dad98a6ef0182eb906c20b147
NickVanBommel <nvanbomm@uwo.ca>
2017-03-04 20:38:35 -0500

    working on adjudication

--- FrontEnd/studentsRecords/app/components/manage-adjudication-rules.js
+++ FrontEnd/studentsRecords/app/components/manage-adjudication-rules.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    actions: {
+        save() {
+
+        },
+        cancel() {
+
+        },
+        addNewRule() {            
+            this.set('code', "");
+            this.set('ruleDescription', "");
+            this.set('ruleParameter', "");
+            this.set('isEditing', true);
+        }
+    }
 });
