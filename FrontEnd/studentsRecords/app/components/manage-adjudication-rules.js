1651427431fe87730f4a987796db9b9131b54366
NickVanBommel <nvanbomm@uwo.ca>
2017-03-05 15:54:58 -0500

    add and delete rule created

--- FrontEnd/studentsRecords/app/components/manage-adjudication-rules.js
+++ FrontEnd/studentsRecords/app/components/manage-adjudication-rules.js
@@ -4,22 +4,39 @@ export default Ember.Component.extend({
 
 
     store: Ember.inject.service(),
-    assessmentCodeModel: Ember.computed(function() {
-        return this.get('store').query('assessment-code',  {});
-    }),
+    assessmentCodeModel: null,
+
+    init() {
+        this._super(...arguments);
+        var self = this;
+        this.get('store').findAll('assessment-code').then(function(codes) {
+            self.set('assessmentCodeModel', codes);
+        });
+    },
 
     actions: {
         save() {
+            var ruleName = this.get('ruleName');
+            var ruleCode = this.get('ruleCode');
+            var newRule = this.get('store').createRecord('assessment-code', {
+                name: ruleName,
+                code: ruleCode
+            });
+            newRule.save();
+            this.set('isEditing', false);
 
         },
         cancel() {
             this.set('isEditing', false);
         },
         addNewRule() {            
-            this.set('code', "");
-            this.set('ruleDescription', "");
+            this.set('ruleName', "");
+            this.set('ruleCode', "");
             this.set('ruleParameter', "");
             this.set('isEditing', true);
+        },
+        deleteRule(rule) {
+           rule.destroyRecord();
         }
     }
 });
