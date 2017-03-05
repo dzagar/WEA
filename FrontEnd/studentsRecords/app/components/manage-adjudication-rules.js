3ab7807af021a25e45a475338dd68eb08dcf758f
NickVanBommel <nvanbomm@uwo.ca>
2017-03-05 18:57:48 -0500

    rule creation front end started and better err page

--- FrontEnd/studentsRecords/app/components/manage-adjudication-rules.js
+++ FrontEnd/studentsRecords/app/components/manage-adjudication-rules.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
 
     store: Ember.inject.service(),
     assessmentCodeModel: null,
+    isAdding: false,
+    isEditing: false,
+    selectedParameterType: null,
+    parameterTypes: null,
 
     init() {
         this._super(...arguments);
@@ -12,6 +16,7 @@ export default Ember.Component.extend({
         this.get('store').findAll('assessment-code').then(function(codes) {
             self.set('assessmentCodeModel', codes);
         });
+        this.set('parameterTypes', [{"value": "YWA", "text": "Yearly weighted average"}, {"value": "CWA", "text": "Cumulative weighted average"}, {"value": "fails", "text": "Number of failed courses"}]);
     },
 
     actions: {
@@ -37,6 +42,58 @@ export default Ember.Component.extend({
         },
         deleteRule(rule) {
            rule.destroyRecord();
+        },
+        addFirstRule(){
+            Ember.$("#addFirst").hide();
+            this.set('isAdding', true);
+        },
+        addParameter() {            
+			Ember.$('.ui.modal').modal('hide');
+      		Ember.$('.ui.modal').remove();
+            this.set('isAdding', false);
+        },
+        cancelRuleAdd() {
+			Ember.$('.ui.modal').modal('hide');
+      		Ember.$('.ui.modal').remove();
+            this.set('isAdding', false);
+        },
+        selectParameterType(newParameterType) {
+            this.set('selectedParameterType', newParameterType)
+            switch (newParameterType){
+                case "YWA":
+                {
+                    Ember.$("#firstParamDesc").text("Minimum yearly weighted Average: ");
+                    Ember.$("#secondParamDesc").text("Maximum yearly weighted Average: ");
+                    Ember.$("#firstParamValue").attr("disabled", false);
+                    Ember.$("#secondParamValue").attr("disabled", false);
+                    break;
+                }
+                case "CWA":
+                {
+                    Ember.$("#firstParamDesc").text("Minimum cumulative weighted Average: ");
+                    Ember.$("#secondParamDesc").text("Maximum cumulative weighted Average: ");
+                    Ember.$("#firstParamValue").attr("disabled", false);
+                    Ember.$("#secondParamValue").attr("disabled", false);
+                    break;
+                }
+                case "fails":
+                {
+                    Ember.$("#firstParamDesc").text("Minimum number of failed courses: ");
+                    Ember.$("#secondParamDesc").text("Minimum number of failed courses: ");
+                    Ember.$("#firstParamValue").attr("disabled", false);
+                    Ember.$("#secondParamValue").attr("disabled", false);
+                    break;
+                }
+
+            }
+            
         }
+    },
+    didRender() {
+        Ember.$('.ui.modal')
+        .modal({
+            closable: false
+        })
+        .modal('show');
     }
 });
