b8d419716cbe4735db572a7e8fba147c2b42cb1e
NickVanBommel <nvanbomm@uwo.ca>
2017-03-09 21:10:10 -0500

    fixed term code import

--- BackEnd/studentsRecords/models/termCode.js
+++ BackEnd/studentsRecords/models/termCode.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var termCodeSchema = mongoose.Schema(
     {
         name: String,
-        adjudications: {type: mongoose.Schema.ObjectId, ref: 'Adjudication'},
-        terms: {type: mongoose.Schema.ObjectId, ref: 'Term'}
+        adjudications: [{type: mongoose.Schema.ObjectId, ref: 'Adjudication'}],
+        terms: [{type: mongoose.Schema.ObjectId, ref: 'Term'}]
         // Adjudication
     },
     {
