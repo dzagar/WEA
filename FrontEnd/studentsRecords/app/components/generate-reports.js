3b3df7f21d57ab5ac4375eb0be40130142efb062
darcese <darcese@uwo.ca>
2017-03-14 13:16:34 -0400

    add pie chart template

--- FrontEnd/studentsRecords/app/components/generate-reports.js
+++ FrontEnd/studentsRecords/app/components/generate-reports.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	chartData: Ember.computed(function(){
+  	return {
+  		labels: ["label", "label2"],
+  		datasets: [{
+  			label: "axisLabel",
+  			data: [10,3],
+  			backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'
+            ]
+  		}]
+  	};
+  }),
+  options: {
+  	responsive:true
+  }
 });
