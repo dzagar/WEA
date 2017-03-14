372758d2e969553d540bf177d4e8ce151fc9536c
Tom Machado <tmachad3@uwo.ca>
2017-03-14 14:40:50 -0400

    Got routes working for departments

--- BackEnd/studentsRecords/routes/departments.js
+++ BackEnd/studentsRecords/routes/departments.js
@@ -3,45 +3,123 @@ var router = express.Router();
 var AssessmentCodes = require('../models/assessmentCode');
 var Faculty = require('../models/faculty');
 var Department = require('../models/department');
+var ProgramAdministration = require('../models/programAdministration');
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 var parseJSON = bodyParser.json();
 
 router.route('/')
     .post(parseUrlencoded, parseJSON, function (request, response) {
+        let completed = 0;
+        let failed = false;
+
         var department = new Department(request.body.department);
         if (department.assessmentCodes && department.assessmentCodes.length > 0) {
             let completeAC = 0;
-            let failed = false;
             for (let i = 0; i < department.assessmentCodes.length && !failed; i++) {
                 AssessmentCodes.findById(department.assessmentCodes[i], function (error, assessmentCode) {
                     if (error && !failed) {
                         failed = true;
                         response.send(error);
                     } else if (assessmentCode) {
-                        department.faculty = faculty._id;
+                        assessmentCode.departments.push(department._id);
 
-                        department.save(function(error) {
+                        assessmentCode.save(function(error) {
                             if (error && !failed) {
                                 failed = true;
                                 response.send(error);
                             } else {
-                                completeDepts++;
-                                if (completeDepts === faculty.departments.length && !failed) {
-                                    response.json({faculty: faculty});
+                                completeAC++;
+                                if (completeAC === department.assessmentCodes.length && !failed) {
+                                    completed++
+                                    if (completed === 3 && !failed) {
+                                        response.json({department: department});
+                                    }
                                 }
                             }
                         });
                     } else {
-                        completeDepts++;
-                        if (completeDepts === faculty.departments.length && !failed) {
-                            response.json({faculty: faculty});
+                        completeAC++;
+                        if (completeAC === department.assessmentCodes.length && !failed) {
+                            completed++;
+                            if (completed === 3 && !failed) {
+                                response.json({department: department});
+                            }
                         }
                     }
                 });
             }
         } else {
-            response.json({faculty: faculty});
+            completed++;
+            if (completed === 3 && !failed) {
+                response.json({department: department});
+            }
+        }
+
+        if (department.faculty) {
+            Faculty.findById(department.faculty, function (error, faculty) {
+                if (error && !failed) {
+                    failed = true;
+                    response.send(error);
+                } else if (faculty) {
+                    faculty.departments.push(department._id);
+
+                    faculty.save(function (error) {
+                        if (error && !failed) {
+                            failed = true;
+                            response.send(error);
+                        } else {
+                            completed++;
+                            if (completed === 3 && !failed) {
+                                response.json({department: department});
+                            }
+                        }
+                    });
+                } else {
+                    completed++;
+                    if (completed === 3 && !failed) {
+                        response.json({department: department});
+                    }
+                }
+            });
+        } else {
+            completed++;
+            if (completed === 3 && !failed) {
+                response.json({department: department});
+            }
+        }
+
+        if (department.programAdministration) {
+            ProgramAdministration.findById(department.programAdministration, function (error, progAdmin) {
+                if (error && !failed) {
+                    failed = true;
+                    response.send(error);
+                } else if (progAdmin) {
+                    progAdmin.department = department._id;
+
+                    progAdmin.save(function (error) {
+                        if (error && !failed) {
+                            failed = true;
+                            response.send(error);
+                        } else {
+                            completed++;
+                            if (completed === 3 && !failed) {
+                                response.json({department: department});
+                            }
+                        }
+                    });
+                } else {
+                    completed++;
+                    if (completed === 3 && !failed) {
+                        response.json({department: department});
+                    }
+                }
+            });
+        } else {
+            completed++;
+            if (completed === 3 && !failed) {
+                response.json({department: department});
+            }
         }
     })
     .get(parseUrlencoded, parseJSON, function (request, response) {
