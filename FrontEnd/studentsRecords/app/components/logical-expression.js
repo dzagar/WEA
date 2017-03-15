aca6bbb61df6e067c4cce4a5733bd7129a139e5a
Tom Machado <tmachad3@uwo.ca>
2017-03-15 16:08:00 -0400

    Added test code for parent calling child function

--- FrontEnd/studentsRecords/app/components/logical-expression.js
+++ FrontEnd/studentsRecords/app/components/logical-expression.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+    applyEmphasis: Ember.computed('level', function() {
+        return (this.get('level') % 2) === 1;
+    }),
     operator: null,
     arg1: null,
     arg2: null,
@@ -11,10 +14,19 @@ export default Ember.Component.extend({
     arg2IsLogExp: false, /*Ember.computed('arg2', function() {
         return (typeof(this.get('arg2')) === 'object') && (this.get('arg2') !== null);
     }),*/
+    level: 0,
+    deleteSelf: null,
+    parent: null,
+    test: null,
 
     init() {
         this._super(...arguments);
         console.log('logical-expression init');
+    },
+
+    didInsertElement() {
+        this.set('parent', this);
+        console.log('element inserted');
     },
 
     didRender() {
@@ -37,7 +49,31 @@ export default Ember.Component.extend({
 
     actions: {
         splitArg(argname) {
-            this.set(argname + 'IsLogExp', !this.get(argname + 'IsLogExp'));
+            if (argname == 'arg1')
+            this.set(argname + 'IsLogExp', true);
+            else
+                this.get('test').log();
+            //set arg to object
+        },
+
+        removeArg(argname) {
+            this.set(argname + 'IsLogExp', false);
+            this.set(argname, null);
+        },
+
+        removeSelf() {
+            console.log('removing self');
+            if (this.get('deleteSelf')) {
+                this.get('deleteSelf')();
+            }
+        },
+
+        log() {
+            console.log('Hello World! I am at level ' + this.get('level'));
+        },
+
+        logchild() {
+            this.get('test').send('log');
         }
     }
 });
