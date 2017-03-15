ed9c2e047a8d6b8175fd1f20dd303b9a74b1d0f3
mromao <mromao@uwo.ca>
2017-03-15 18:29:02 -0400

    Created new CourseGrouping models in FrontEnd and BackEnd

---
+++ BackEnd/studentsRecords/models/courseGrouping.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var courseGroupingSchema = mongoose.Schema(
+    {
+        name: String,
+        courseCodes:[{type: mongoose.Schema.ObjectId, ref: 'CourseCode'}]
+    },
+
+    {
+        versionKey: false
+    }
+);
+
+module.exports = mongoose.model('courseGrouping', courseGroupingSchema);
