f47a9b59c354644bf9b153167e14a49b76ef94f2
Tom Machado <tmachad3@uwo.ca>
2017-03-15 10:39:08 -0400

    Fleshed out logExp component

--- FrontEnd/studentsRecords/app/components/logical-expression.js
+++ FrontEnd/studentsRecords/app/components/logical-expression.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    operator: null,
+    arg1: null,
+    arg2: null,
+    arg1IsLogExp: Ember.computed('arg1', function() {
+        return (typeof(this.get('arg1')) === 'object') && (this.get('arg1') !== null);
+    }),
+    arg1IsLogExp: Ember.computed('arg2', function() {
+        return (typeof(this.get('arg2')) === 'object') && (this.get('arg2') !== null);
+    }),
+
+    init() {
+        this._super(...arguments);
+        console.log('logical-expression init');
+    },
+
+    didRender() {
+        Ember.$('#arg1dropdown')
+        .dropdown({
+            direction: 'downward',
+            fullTextSearch: 'exact'
+        });
+        Ember.$('#opdropdown')
+        .dropdown({
+            direction: 'downward',
+            fullTextSearch: 'exact'
+        });
+        Ember.$('#arg2dropdown')
+        .dropdown({
+            direction: 'downward',
+            fullTextSearch: 'exact'
+        })
+    },
+
+    actions: {
+        splitArg(argname) {
+            console.log(argname);
+        }
+    }
 });
