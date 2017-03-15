2bd5c85b89566753ddb0fd8629b3ff2a76b121a7
dzagar <dzagar@uwo.ca>
2017-03-15 17:39:18 -0400

    Began change to new Add Params UI

--- FrontEnd/studentsRecords/app/components/logical-expression.js
+++ FrontEnd/studentsRecords/app/components/logical-expression.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
     applyEmphasis: Ember.computed('level', function() {
         return (this.get('level') % 2) === 1;
     }),
+    logLinks: null,  //logical link (any or all)
+    department: null,   //what department this rule applies to
+    fields: null,    //fields (argument 1 of boolean)
+    operators: null,    //possible boolean operators (greater than, less than, etc)
     operator: null,
     arg1: null,
     arg2: null,
@@ -22,29 +26,79 @@ export default Ember.Component.extend({
     init() {
         this._super(...arguments);
         console.log('logical-expression init');
+        var logLinks = [
+            {
+                "id": 0,
+                "text": "all"
+            },
+            {
+                "id": 1,
+                "text": "any"
+            },
+        ];
+        var departments = [ //STRICTLY FOR TEST PURPOSES. WOULD LOAD IN DEPARTMENTS.
+            {
+                "id": 0,
+                "name": "all departments"
+            },
+            {
+                "id": 1,
+                "name": "Software Engineering"
+            }
+        ];
+        var fields = [  //TESTER
+            {
+                "id": 0,
+                "name": "Yearly weighted average"
+            },
+            {
+                "id": 1,
+                "name": "Cumulatively weighted average"
+            },
+            {
+                "id": 2,
+                "name": "Number of failed credits"
+            }, 
+            {
+                "id": 3,
+                "name": "Number of credits"
+            }
+        ];
+        var operators = [
+            {
+                "id": 0,
+                "name": "is greater than"
+            },
+            {
+                "id": 1,
+                "name": "is less than"
+            },
+            {
+                "id": 2,
+                "name": "is greater than or equal to"
+            },
+            {
+                "id": 3,
+                "name": "is less than or equal to"
+            },
+            {
+                "id": 4,
+                "name": "is equal to"
+            },
+            {
+                "id": 5,
+                "name": "is not equal to"
+            }
+        ];
+        this.set('fields', fields);
+        this.set('operators', operators);
+        this.set('logLinks', logLinks);
+        this.set('departments', departments);
     },
 
     didInsertElement() {
         this.set('parent', this);
         console.log('element inserted');
-    },
-
-    didRender() {
-        Ember.$('#arg1dropdown')
-        .dropdown({
-            direction: 'downward',
-            fullTextSearch: 'exact'
-        });
-        Ember.$('#opdropdown')
-        .dropdown({
-            direction: 'downward',
-            fullTextSearch: 'exact'
-        });
-        Ember.$('#arg2dropdown')
-        .dropdown({
-            direction: 'downward',
-            fullTextSearch: 'exact'
-        })
     },
 
     actions: {
