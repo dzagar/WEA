21a2af46518629e9533bed1434faf06b77270fa3
Tom Machado <tmachad3@uwo.ca>
2017-03-17 18:00:19 -0400

    Removed test button from add student Also fixed scrolling sometimes not working

--- FrontEnd/studentsRecords/app/components/add-student.js
+++ FrontEnd/studentsRecords/app/components/add-student.js
@@ -110,9 +110,6 @@ export default Ember.Component.extend({
 			this.set('notDONE', false);
 			Ember.$('.ui.modal').modal('hide');
       		//Ember.$('.ui.modal').remove();
-		},
-		test() {
-			console.log('Student number: ' + this.get('number'));
 		}
 	},
 
