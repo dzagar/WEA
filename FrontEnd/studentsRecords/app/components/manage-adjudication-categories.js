ff39a6ada6e42d91c36d17a1ffdd991f0072e157
NickVanBommel <nvanbomm@uwo.ca>
2017-03-17 19:34:12 -0400

    dope help from Hannis

--- FrontEnd/studentsRecords/app/components/manage-adjudication-categories.js
+++ FrontEnd/studentsRecords/app/components/manage-adjudication-categories.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    adjudicationCategories: null,
+    newCategoryName: null,
+	store: Ember.inject.service(),
+	init(){
+		this._super(...arguments);
+        var self = this;
+        this.get('store').findAll('adjudication-category').then(function(records) {
+            self.set('adjudicationCategories', records);
+        });
+    },
+
+    actions: {
+        saveCategory(adjudicationCategory){
+            adjudicationCategory.save();
+        },
+        addCategory(){
+            var self = this;
+            var newCategoryName = this.get('newCategoryName');
+            var newCategory = this.get('store').createRecord('adjudication-category', {
+                name: newCategoryName
+            });
+            newCategory.save().then(function(){
+                self.set('newCategoryName', "");
+            });
+        },
+        deleteCategory(adjudicationCategory){
+            adjudicationCategory.destroyRecord();
+        }
+    }
 });
