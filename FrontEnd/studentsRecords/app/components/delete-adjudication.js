118353ada0fd2c17bc97c71c9f14efd888f350b7
dzagar <dzagar@uwo.ca>
2017-03-21 10:09:18 -0400

    Completed front-end work
    Working through back-end bugs

--- FrontEnd/studentsRecords/app/components/delete-adjudication.js
+++ FrontEnd/studentsRecords/app/components/delete-adjudication.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	adjudication: null,
+	showWindow: null,
+	store: Ember.inject.service(),
+
+	actions: {
+		deleteAdjudication: function(){
+			this.get('adjudication').destroyRecord();
+			this.set('showWindow', false);
+			Ember.$('.ui.modal').modal('hide');
+      		Ember.$('.ui.modal').remove();
+
+		},
+		cancel: function(){
+			this.set('showWindow', false);
+			Ember.$('.ui.modal').modal('hide');
+      		Ember.$('.ui.modal').remove();
+		}
+	},
+
+    didRender() {
+    Ember.$('.ui.modal')
+      .modal({
+        closable: false,
+      })
+      .modal('show');
+  	}
 });
