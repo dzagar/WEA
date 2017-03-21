54616ce4f8b094e1c07b75bb2bc12eccff74e650
dzagar <dzagar@uwo.ca>
2017-03-21 08:57:25 -0400

    Added delete window for adjudication, added component to student-data-entry
    Starting hbs and functions for tab

--- FrontEnd/studentsRecords/app/components/student-adjudication-tab.js
+++ FrontEnd/studentsRecords/app/components/student-adjudication-tab.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	currentStudent: null,
+	showDeleteConfirmation: false,
+	adjudications: null,
+	assessmentCodes: null,
+	loadingAdjudications: false,
+	newDateValue: null,
+	newNoteValue: null,
+	newTermCodeValue: null,
+	newAssessmentCodeValue: null,
+	selectedAdjudicationID: null,
+	selectedAssessmentCodeID: null,
+	store: null,
 
+	
 });
