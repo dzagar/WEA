358e0dd2e5e19b9d7ed835d5ae70df455134b92a
dzagar <dzagar@uwo.ca>
2017-03-27 23:53:48 -0400

    Reformatted nabber and added conditional to Admin Portal link

--- FrontEnd/studentsRecords/app/controllers/application.js
+++ FrontEnd/studentsRecords/app/controllers/application.js
@@ -3,6 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
     routing: Ember.inject.service('-routing'),
+
+    ADM01IsPermitted: function(){
+    	var authentication = this.get('oudaAuth');
+	    if (authentication.getName === "Root") {
+	      return true;
+	    } else {
+	      return (authentication.get('userCList').indexOf("ADM01") >= 0);
+	    }
+    }.property('login').volatile(),
+
     actions: {
         logout() {
             this.get('oudaAuth').close();
--- FrontEnd/studentsRecords/app/routes/application.js
+++ FrontEnd/studentsRecords/app/routes/application.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  
   beforeModel () {
     var authentication = this.get('oudaAuth');
     var self = this;
     authentication.fetch().then(
       function () {
+        self.controller.set('login', true);
         self.transitionTo('home');
       },
       function (error) {
